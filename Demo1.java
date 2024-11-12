94be8808655c50dbe83dfc63a67103e77863c91e
Gulshan Yadav <135387702+GulshanYadav9684@users.noreply.github.com>
2024-11-12 20:30:50 +0530

    IC

---
+++ Demo1.java
@@ -0,0 +1,23 @@
+class Demo1
+{
+	public static void main(String ar[])
+	{
+		// System.out.println("Git");
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+>>>>>>> new
+=======
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+		System.out.println("Github");
+>>>>>>> new1
+	}
+}
